0c225a812ad459131f9e3e0cb05a5ae36ac48751
Jinping Wu <jinping.wu@verisilicon.com>
2021-07-30 16:23:52 +0800

    OPTEEV7 modify script

--- modules/opteev7/optee_runqemu.sh
+++ modules/opteev7/optee_runqemu.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+qemu_option=
+if [[ $1  = "--gdb" ]]; then
+    qemu_option+="run-only-gdb"
+    echo "enable gdb, please run script './rungdb', and enter c "
+else
+    qemu_option+="run-only"
+    echo "not use gdb, just run"
+fi
+
 cd build || exit
-#make run-only
-make run-only-gdb
+
+make ${qemu_option}
 
